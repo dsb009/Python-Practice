0fc44adf5f938d8bdde610b307ea25e06eb52361
dsb009 <singh.debashish4@gmail.com>
2022-07-18 09:24:38 +0530

    working Hex color code identifier

--- Hex-Color-Code.py
+++ Hex-Color-Code.py
@@ -1,21 +1,21 @@
-import sys
-
-def check_if_color_code(*args):
-    for i in args:
-        print(i[0])
-        print(i[1])
-        print(i[2])
-        print(i[3])
-        print(i[4])
-        # if i[1] =='#':
-        #     print(i + "first if")
-        #     if 3 <= len(i) <= 6:
-        #         print(i+ "second if")
-        #         if str(i) in (1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F','a','b','c','d','e','f'):
-        #             print(i + "third if") 
+'''
+Problem Statement: https://www.hackerrank.com/challenges/hex-color-code/problem?isFullScreen=false
+'''
+import sys,time,re
 if __name__ == '__main__':
+    start = time.time()
     number_of_lines = int(input("Enter the total number of lines of code:" ))
-    #https://stackoverflow.com/questions/58612826/how-to-read-n-lines-at-a-time-from-stdin
+    '''
+    explanation for below line in:
+    https://stackoverflow.com/questions/58612826/how-to-read-n-lines-at-a-time-from-stdin
+    
+    
+    '''
     code=[sys.stdin.readline().strip() for _ in range(number_of_lines)]
-    # print(type(code))
-    check_if_color_code(code)
+    for i in code:
+       color_code = re.findall('[\s:](#[a-f0-9]{6}|#[a-f0-9]{3})[\s:;,)]',i,re.IGNORECASE)
+       #print(*color_code)
+       for j in color_code:
+        print(j)
+    end = time.time()
+    print("Time taken in {}".format(start-end))
