dfd2b1b82212be936665d125e8b4cb5307eb608b
dsb009 <singh.debashish4@gmail.com>
2022-07-22 12:28:35 +0530

    working html parser 2

--- html_parser-1.py
+++ html_parser-1.py
@@ -4,26 +4,25 @@ Problem Statement:
 https://www.hackerrank.com/challenges/html-parser-part-1/problem
 '''
 from html.parser import HTMLParser
-import sys
-
+import time
 
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag: str, attrs):
-        print("Start : ", tag)
+        print("Start :", tag.strip())
         for attr in attrs:
             print("-> {} > {}".format(attr[0], attr[1]))
-    def handle_endtag(self, tag: str,attrs):
-        print("End   : ",tag)
-        for attr in attrs:
-            print("-> {} > {}".format(attr[0], attr[1]))
+    def handle_endtag(self, tag: str):
+        print("End   :",tag.strip())
     def handle_startendtag(self, tag: str, attrs):
-        print("Empty : ", tag)
+        print("Empty :", tag.strip())
+        for name,value in attrs:
+            print("->",name+" >",value)
 parser = MyHTMLParser()
 
+start = time.time()
 n = int(input())
 for _ in range(n):
-    code =sys.stdin.readline()
-    parser.feed(code)
+    parser.feed(input())
+end= time.time()
 
-    
-   
+print(start-end)
---
+++ html_parser-2.py
@@ -0,0 +1,22 @@
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data: str) -> None:
+        if '\n' in data:
+            print(">>> Multi-line Comment")
+            print(data.strip())
+        else:
+            print(">>> Single-line Comment")
+            print(data.strip())
+    def handle_data(self, data: str) -> None:
+        if '\n' in data:
+        print(type(data))
+
+html = ""       
+for i in range(int(input())):
+    html += input().strip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
